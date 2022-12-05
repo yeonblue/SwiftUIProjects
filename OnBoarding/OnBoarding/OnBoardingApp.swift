dda6900d47bb65585085ca15d3f6ee92e5543f17
최서연 <yeondublue@naver.com>
2022-12-05 16:48:03 +0900

    prepare OnBoarding Project

---
+++ OnBoarding/OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by yeonBlue on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnBoarding/README.md
@@ -0,0 +1,2 @@
+# 1. OnBoarding
+Custom OnBoarding Home Screen using SwiftUI
