b0c96dfc8e46cb8107bea81e2b5f62285920f0b2
yeonblue <yeondublue@naver.com>
2022-12-24 17:29:27 +0900

    add game functions

--- SlotMachine/SlotMachine/Views/ContentView.swift
+++ SlotMachine/SlotMachine/Views/ContentView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: - Properties
+    @AppStorage("highScore") private var highScore = 0
+    
     @State private var showingInfoView = false
     @State private var reels = [0, 1, 2]
-    @State private var highScore = 0
     @State private var coins = 100
     @State private var betAmount = 10
+    @State private var showModal = false
     
     @State private var isActiveBet10 = true
     @State private var isActiveBet20 = false
@@ -64,6 +66,18 @@ struct ContentView: View {
         betAmount = 10
         isActiveBet20 = false
         isActiveBet10 = true
+    }
+    
+    func isGameOver() {
+        if coins <= 0 {
+            showModal = true
+        }
+    }
+    
+    func resetGame() {
+        highScore = 0
+        coins = 100
+        activeateBet10()
     }
     
     // MARK: - Body
@@ -144,6 +158,7 @@ struct ContentView: View {
                     Button {
                         self.spinReels()
                         self.checkWinning()
+                        self.isGameOver()
                     } label: {
                         Image("gfx-spin")
                             .renderingMode(.original)
@@ -197,7 +212,7 @@ struct ContentView: View {
             }
             .overlay(alignment: .topLeading, content: {
                 Button {
-                    
+                    self.resetGame()
                 } label: {
                     Image(systemName: "arrow.2.circlepath.circle")
                 }
@@ -215,6 +230,70 @@ struct ContentView: View {
             .frame(maxWidth: 720)
             .sheet(isPresented: $showingInfoView) {
                 InfoView()
+            }
+            .blur(radius: showModal ? 5 : 0, opaque: false)
+            
+            if showModal {
+                ZStack {
+                    Color.colorTransparentBlack
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    VStack(spacing: 0) {
+                        HStack {
+                            Spacer()
+                            Text("Game Over".uppercased())
+                            Spacer()
+                        }
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.heavy)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.colorPink)
+                        
+                        Spacer()
+                        
+                        // Message
+                        VStack(alignment: .center, spacing: 16) {
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+                            Text("Bad Luck! You lost all of the coins.\nLet's play again!")
+                                .font(.system(.body, design: .rounded))
+                                .multilineTextAlignment(.center)
+                                .lineLimit(2)
+                                .foregroundColor(.gray)
+                                .layoutPriority(1)
+                            
+                            Button {
+                                self.showModal = false
+                                self.coins = 100
+                            } label: {
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .tint(.colorPink)
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 2)
+                                            .foregroundColor(.colorPink)
+                                    )
+                            }
+
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(minWidth: 280, idealWidth: 280, maxWidth: 320,
+                           minHeight: 260, idealHeight: 280, maxHeight: 320,
+                           alignment: .center)
+                    .background(.white)
+                    .cornerRadius(20)
+                    .shadow(color: .colorTransparentBlack, radius: 8)
+                }
             }
         }
     }
